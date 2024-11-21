6d5c4384a1333332fcf377a4d5256f6ab14bf834
Dobledroid <carlosexequiel360@gmail.com>
2024-11-20 22:12:11 -0600

    implementación de Sentry para monitoreo

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'sportApp',
+  appId: 'com.sportgymcenter.app',
+  appName: 'Sport Gym Center',
   webDir: 'www'
 };
 
